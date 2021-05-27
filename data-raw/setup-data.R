16bb2654a3ea63a11c3e00be59a807fa6e101b18
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2021-05-27 11:01:34 +0200

    update data

--- data-raw/setup-data.R
+++ data-raw/setup-data.R
@@ -1,9 +1,13 @@
 
-library(sepsr)
+library(ricu)
 
-target_dir <- "inst/extdata"
+target_dir <- file.path("inst", "extdata")
+
+if (dir.exists(target_dir)) {
+  unlink(target_dir, recursive = TRUE)
+}
 
 dir.create(target_dir, recursive = TRUE)
 
-download_eicu(demo = TRUE, dest = target_dir, version = "2.0")
-import_eicu(demo = TRUE, dir = target_dir)
+download_src("eicu_demo", target_dir)
+import_src("eicu_demo", target_dir, cleanup = TRUE)
